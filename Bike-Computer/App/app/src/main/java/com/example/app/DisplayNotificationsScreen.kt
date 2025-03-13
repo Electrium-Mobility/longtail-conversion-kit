b60dff3e0e6eb19c25bf1f344d1f32bcdeaed9c2
GavinPinto <gavinpinto620@gmail.com>
2025-03-13 17:54:17 -0400

    increase size of direction arrow

--- Bike-Computer/App/app/src/main/java/com/example/app/DisplayNotificationsScreen.kt
+++ Bike-Computer/App/app/src/main/java/com/example/app/DisplayNotificationsScreen.kt
@@ -3,14 +3,30 @@ package com.example.app
 import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,6 +69,7 @@ private fun DirectionsDisplay() {
         directionIcon?.let {
             Image (
                 bitmap = it.asImageBitmap(),
+                modifier = Modifier.size(200.dp),
                 contentDescription = "Direction Arrow",
             )
         }
